acbf590df0f17c82b8cde403db06cb45d023c494
RomStark <rgtarkovskiy@edu.hse.ru>
2023-09-30 06:11:02 +0300

    initial

---
+++ WBTest/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  WBTest
+//
+//  Created by Al Stark on 30.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
